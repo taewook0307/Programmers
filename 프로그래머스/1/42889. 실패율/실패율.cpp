0a2a01bf76cdc40926ed3bf4e3952824cad99d4c
Lard <127205619+taewook0307@users.noreply.github.com>
2024-08-14 17:12:21 +0900

    [level 1] Title: 실패율, Time: 0.87 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.cpp
+++ 프로그래머스/1/42889. 실패율/실패율.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <map>
+#include <algorithm>
+
+bool Compare(const std::pair<float, int>& _LeftPair, const std::pair<float, int>& _RightPair)
+{
+    if (_LeftPair.first == _RightPair.first)
+    {
+        return _LeftPair.second < _RightPair.second;
+    }
+
+    return _LeftPair.first > _RightPair.first;
+}
 
 std::vector<int> solution(int N, std::vector<int> stages) {
-    std::vector<int> answer(N);
+    std::vector<int> answer;
+    answer.reserve(N);
 
     // 실패횟수
     std::vector<int> FailureCount(N);
@@ -20,30 +30,38 @@ std::vector<int> solution(int N, std::vector<int> stages) {
     }
 
     int UserCount = stages.size();
-    // 실패율과 스테이지 번호 저장용 multimap
-    std::multimap<float, int, std::greater<>> FailureRate;
+
+    std::vector<std::pair<float, int>> FailureRate;
+    FailureRate.reserve(N);
 
     for (int i = 0; i < N; ++i)
     {
-        if (UserCount < 0)
+        if(UserCount < 0)
         {
             break;
+        }
+        
+        if (UserCount == 0)
+        {
+            FailureRate.push_back(std::make_pair(0.0f, i + 1));
+            continue;
         }
 
         // 실패율
         float Failure = static_cast<float>(FailureCount[i]) / static_cast<float>(UserCount);
-        // 계산된 실패율과 stage 번호 insert
-        FailureRate.insert(std::make_pair(Failure, i + 1));
-        // 전체 유저 수에서 실패한 횟수 빼기
+
+        // 계산된 실패율과 stage 번호 push
+        FailureRate.push_back(std::make_pair(Failure, i + 1));
+
+        // 전체 유저 수에서 실패한 인원수 빼기
         UserCount -= FailureCount[i];
     }
 
-    int i = 0;
+    sort(FailureRate.begin(), FailureRate.end(), Compare);
 
     for (std::pair<float, int> CurIter : FailureRate)
     {
-        answer[i] = CurIter.second;
-        ++i;
+        answer.push_back(CurIter.second);
     }
 
     return answer;
