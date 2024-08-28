b2773fb84599c6438b8fb4a74776795c8322ba2f
Lard <127205619+taewook0307@users.noreply.github.com>
2024-08-28 11:13:56 +0900

    [level 1] Title: 문자열 나누기, Time: 0.06 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -7,33 +7,39 @@ int solution(std::string s)
 
     int Length = static_cast<int>(s.size());
 
+    // 초기값
     char x = s[0];
-    int Same = 0;
-    int NotSame = 0;
+    int Same = 0;       // 문자가 같을 경우의 갯수
+    int NotSame = 0;    // 문자가 같지 않을 경우의 갯수
 
     for (int i = 0; i < Length; ++i)
     {
+        // 체크하는 문자가 x와 같을 경우
         if (s[i] == x)
         {
             ++Same;
         }
+        // 체크하는 문자가 x와 다를 경우
         else
         {
             ++NotSame;
         }
 
+        // 같은 문자와 같지 않는 문자 수가 서로 같을 경우
         if (Same == NotSame)
         {
             ++answer;
             Same = 0;
             NotSame = 0;
             
+            // 오버플로우 방지
             if (Length > i + 1)
             {
                 x = s[i + 1];
             }
         }
 
+        // 마지막 문자 체크
         if (i == Length - 1 && Same != NotSame)
         {
             ++answer;
