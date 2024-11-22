a574635ddc2caede25306fd204daa8ffcdc5be8e
Lard <127205619+taewook0307@users.noreply.github.com>
2024-11-22 15:06:36 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+
+int solution(int n)
+{
+    if (n < 2)
+    {
+        return n;
+    }
+
+    int answer = 1 + n;
+
+    int LoopCount = sqrt(n);
+
+    for (int i = 2; i <= LoopCount; ++i)
+    {
+        if (n % i == 0)
+        {
+            if (n / i == i)
+            {
+                answer += i;
+            }
+            else
+            {
+                answer += i;
+                answer += n / i;
+            }
+        }
+    }
+
+    return answer;
+}
